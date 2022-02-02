7d84505435907f4b88d665d249b8826ad41fbefb
soblab <soblab@Whit216-07.it.farmingdale.edu>
2022-02-02 10:11:29 -0500

    TextField code added

--- src/main/java/com/mycompany/helloworld/PrimaryController.java
+++ src/main/java/com/mycompany/helloworld/PrimaryController.java
@@ -3,13 +3,19 @@ package com.mycompany.helloworld;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
 
     @FXML
+    private TextField textFieldMessage;
+
+    @FXML
     private void handleShowMessageButton() {
         System.out.println("Button was pressed...");
-        labelmessage.setText("Button was pressed!!!");
+        String message = textFieldMessage.getText();
+        //gets text from text field
+        labelmessage.setText(message);
     }
 
     @FXML
